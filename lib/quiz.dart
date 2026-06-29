69dda9912df8ad6ded319250943b5bbab8b343b4
Othon Gustavo <othon.gustavo92@gmail.com>
2026-06-29 14:23:49 -0300

    Initial commit - Quiz App Flutter completo

---
+++ lib/quiz.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/result_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
+  Widget? activeScreen;
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = ResultScreen(
+          chosenAnswers: selectedAnswers,
+          onRestart: restartQuiz,
+        );
+      });
+    }
+  }
+
+  void restartQuiz() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = StartScreen(switchScreen);
+    });
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = QuestionsScreen(onSelectedAnswer: chooseAnswer);
+    });
+  }
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: const Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.help_sharp, color: Colors.white),
+              SizedBox(width: 8),
+              Text('Flutter Quiz', style: TextStyle(color: Colors.white)),
+            ],
+          ),
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 103, 58, 183),
+                Color.fromARGB(255, 81, 45, 168)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen!,
+        ),
+      ),
+    );
+  }
+}
